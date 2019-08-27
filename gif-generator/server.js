f485dd66d1c2a6b83b3234b5e974cf07b874570c
Ryan Bateman <secretdark@gmail.com>
2019-08-27 10:41:15 +0100

    Added Tweeting, tidied how media filebuffer was assembled, made error handling a little neater, made tweeting a parameter to be passed in

--- gif-generator/server.js
+++ gif-generator/server.js
@@ -9,8 +9,8 @@ import dotenv from 'dotenv'
 import logger from 'morgan'
 import fs from 'fs'
 
-var style = JSON.parse(fs.readFileSync('teststyle.json', 'UTF-8'))
 // style JSON file with MapBox style.  Can also be opened and read instead of imported.
+var style = JSON.parse(fs.readFileSync('teststyle2.json', 'UTF-8'))
 
 // Constants
 dotenv.config()
@@ -36,54 +36,101 @@ var client = new twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-app.post('/', (req, res) => {
 
-	res.type("image/gif")
+app.post('/', (req, res) => {
+	console.log("-------------------------")
+	console.log("Request received")
 	const layers = req.body.layers
 	const title = req.body.title
 	const description = req.body.description
 	const coords = req.body.coords
 	const zoom = req.body.zoom	
+	const publishTweet = req.body.publishTweet
 	const width = 300
 	const height = 300
 
 	const canvas = createCanvas(width, height)
 	const ctx = canvas.getContext('2d')
 	const encoder = new GIFEncoder(width, height)
+	// The renderer takes lon/lat for some reason?
 	const center = [coords[1], coords[0]]
-
-	console.log(`Layer count: ${layers.length} passed, ${style.layers.length} in file`)	
 	var promises = new Array(layers.length)
 
-	layers.forEach(function (layerToPop, index) {
-		console.log("Layer: " + layerToPop)
+	console.log("Making promises...")
+	// For each layer, set up a Promise to render it, using our parameters
+	layers.forEach(function (layerToKeep, index) {
 		const styleClone = JSON.parse(JSON.stringify(style))
+
+		// Keep only the layer that is the one we're rendering OR 
+		// wasn't included in our list of passed layer IDs
 		styleClone.layers = styleClone.layers.filter(layer =>
-			//if (layer.id == layerToPop) 
-			//	console.log(`Layer ID comparison: ${layer.id} to ${layerToPop}`)
-			layer.id !== layerToPop
+			layer.id == layerToKeep || !layers.includes(layer.id)
 		)
-		console.log(`Layers after filter: ${styleClone.layers.length}`)
 		promises[index] = render(styleClone, width, height, { zoom, center, token: `${ACCESS_TOKEN}` })	
 	})
 
-	// Create GIF
-	encoder.createReadStream().pipe(res)
+	// Create the stream to render the GIF
+	var stream = encoder.createReadStream()
+	let chunks = []
+	var fileBuffer;
+
+	// Each time there's a new chunk/frame added to the GIF, prep it for the twitter buffer
+	// Pretty sure we could skip this step if I could get the stream outputting a Buffer
+	// directly. 
+	stream.on('data', (chunk) => {
+		console.log("Chunking data...")
+		chunks.push(chunk);
+	})
+
+	// Once we've finished creating the GIF, upload it to Twitter
+	stream.once('end', () => {
+		console.log("Data chunked!")
+		fileBuffer = Buffer.concat(chunks);
+		if (publishTweet) {
+			console.log("Opting to tweet...")
+			client.post('media/upload', {media: fileBuffer}, function(error, media, response) {
+			  if (!error) {
+			    var status = {
+			      status: "GIF up",
+			      media_ids: media.media_id_string // Pass the media id string
+			    }
+
+			    client.post('statuses/update', status, function(error, tweet, response) {
+			      if (!error) {
+			        console.log("Successfully tweeted")
+			      }
+			    })
+			  } else {
+			  	console.log(error.message)
+			  }
+			})
+		}
+	})
+
+	// Start the GIF encoder
 	encoder.start()
 	encoder.setRepeat(0)   // 0 for repeat, -1 for no-repeat
 	encoder.setDelay(500)  // frame delay in ms
 	encoder.setQuality(10) // image quality. 10 is default.
 
+	// For each set up render of a layer, return the image and create a GIF frame
 	Promise.all(promises).then(function(values) {
 		values.forEach(function (value, index) {
 			const img = new Image
 			img.src = values[index]
 		 	ctx.drawImage(img, 0, 0, width, height)
 			encoder.addFrame(ctx)
+			console.log("Adding frame to GIF")
 		})
+
+		// If all rendered correctly, set our response type and pipe the result out
+		res.type("image/gif")
+		stream.pipe(res)
 		encoder.finish()
+		console.log("-------------------------")
 	}).catch(function (err) {
-    	console.log(err.message)
+    	res.status(403).send("Error retrieving map files.")
+    	console.log("-------------------------")
 	})
 })
 
